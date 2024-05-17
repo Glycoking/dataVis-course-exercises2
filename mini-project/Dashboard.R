55cedffb2e3add25d4c9531a9b4721ed846a8b49
Glycoking <134965902+Glycoking@users.noreply.github.com>
2024-05-17 11:44:36 +0200

    timmoC

--- mini-project/Dashboard.R
+++ mini-project/Dashboard.R
@@ -2,6 +2,11 @@ library(shiny)
 library(plotly)
 library(tidyverse)
 library(shinydashboard)
+load("data/aggData.rda")
+data <- agg_dataNum
+
+# Shiny -------------------------------------------------------------------
+
 
 ui <- dashboardPage(skin = "black",
   dashboardHeader(title = "Hospital stats"),
@@ -18,7 +23,7 @@ ui <- dashboardPage(skin = "black",
                 box(plotOutput("plot1", height = 250)),
                 box(
                   title = "Slider",
-                  sliderInput("slider", "Antal observationer:", 1, 100, 50)
+                  sliderInput("slider", "Antal observationer:", 1, 5000, 100)
                 )
               )
       ),
@@ -30,20 +35,26 @@ ui <- dashboardPage(skin = "black",
 )
 
 server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
+  data <- data %>% filter(subGroup == "gender")
+
+
   output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+    data <- data[seq_len(input$slider)]
+    plot(data = data, aes(x=subGroupVal)) + geom_bar(fill = "yellow2", colour="black")
   })
 }
 
-shinyApp(ui, server)
-load("data/aggData.rda")
-data <- agg_dataNum
-glimpse(data)
+# ;) ----------------------------------------------------------------------
 
-x<- data$quarter
-ggplot(data = data, aes(y=subGroupVal)) + geom_bar(fill = "yellow", colour="black")
 
+shinyApp(ui, server) #Dashboard
+
+#glimpse(data)
+
+x <- data %>% filter(subGroup == "gender")
+
+ggplot(data = subset(x,!is.na(subGroupVal)), aes(x=subGroupVal)) + geom_bar(
+  fill = "yellow2", colour="black") + theme_classic()
+
+ggplot(data = subset(x,!is.na(subGroupVal)), aes(x=subGroupVal)) + geom_bar(fill="darkblue",width = 0.6) + theme_classic()
        
