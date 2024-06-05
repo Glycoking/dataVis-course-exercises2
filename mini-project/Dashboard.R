b24b818da8ee32366761a82e89cae23b518bb2ab
Glyco <glycoking@gmail.com>
2024-06-05 19:50:37 +0200

    2 graphs you can interact with

--- mini-project/Dashboard.R
+++ mini-project/Dashboard.R
@@ -2,70 +2,83 @@ library(shiny)
 library(plotly)
 library(tidyverse)
 library(shinydashboard)
-#setwd("~/Github/dataVis-course-exercises2/mini-project")
-#setwd("~/Github/dataVis-course-exercises2/mini-project")
+library(ggplot2)
+setwd("~/Github/dataVis-course-exercises2/mini-project")
 load("data/aggData.rda")
 data <- agg_dataNum
 
 # Shiny -------------------------------------------------------------------
-
-
 ui <- dashboardPage(skin = "black",
-  dashboardHeader(title = "Hospital stats"), #Top
+  dashboardHeader(title = "Dashboard for clinicians"), #Top
   dashboardSidebar(),
   dashboardBody(
-    #box(plotOutput("quarter_plot"), width = 8),# Plot
-    #box(selectInput("quarter", "Quarters", c("all", "Q1","Q2","Q3","Q4")), width = 4),
-    #box(selectInput("gender", "Gender", c("all", "female","male")), width = 4)),
-    box(plotOutput("Quality_indicator")),
-    box(selectInput(inputId = "KPI", label = "KPI", choices = c("discharge_any_anticoagulant","door_to_groin", 
+    box(selectInput(inputId = "KPI", label = "Select a KPI", choices = c("discharge_any_anticoagulant","door_to_groin", 
                                     "door_to_needle","discharge_any_antiplatelet",
                                     "imaging_done","dysphagia_screening_type", 
                                     "thrombolysis","hospitalized_in"))),
+    box(checkboxInput("Color", "Colorblind", value = FALSE)),
+    #box(sliderInput("Year_slider", "Set year range", value=c(2016,2024), min = 2016, max = 2024)),
+    box(selectInput(inputId ="quarter",label = "Quarters", c("Q1","Q2","Q3","Q4")), width = 4),
+    #box(dateRangeInput("Year", "YQ", c("all","2016","2017", "2018","2019","2020","2021", "2022", "2023")), width = 4),
+    #box(selectInput("gender", "Gender", c("all", "female","male")), width = 4)),
+    box(plotOutput("KPI_plot"), width = 6, height = 6),
+    box(plotOutput("Scatter")),
     #box(checkboxGroupInput(inputID = "KPI2", label = "KPI2", choices = c("discharge_any_anticoagulant","door_to_groin", 
     #                                                                    "door_to_needle","discharge_any_antiplatelet",
     #                                                                    "imaging_done","dysphagia_screening_type", 
-    #                                                                    "thrombolysis","hospitalized_in"))),
+    #                                                                    "thrombolysis","hospitalized_in"))),'
+    # data input1 <= ?? <= input2
 ))
-# Lav et linjeplot, #Lav en tjekboks
-
+# Lav et linjeplot,
+#Plot der viser KPI forskelle fra år til år
 server <- function(input, output) {
-  
-  output$Quality_indicator <- renderPlot({
-    x <- data["QI"] %>% filter(data["QI"] == input$KPI)
-    ggplot(data = x, aes(x = input$KPI)) + geom_bar(fill = "darkblue")
-  })
-  output$Quality_indicator <- renderPlot({
-    x <- data["QI"] %>% filter(data["QI"] == input$KPI)
-    ggplot(data = x, aes(x = input$KPI)) + geom_bar(fill = "darkblue")
-  })
-  
-  x <- data %>% filter(subGroup == "gender")
-  output$quarter_plot <- renderPlot({ #Plot
-    
-    ggplot(data = data[[input$gender]]%>% filter(subGroup == "gender"), aes(x=subGroupVal)) + geom_bar()#Argumenter
-    #plot(x$year, x$qual4Gold,xlab = "gg", ylab = "Feature")
-    #ggplot(data = subset(x,!is.na(subGroupVal)), aes(x=subGroupVal)) + geom_bar(fill="darkblue",width = 0.6) + theme_classic()
-  })
-}
+  output$KPI_plot <- renderPlot({
+    color1 <- "blue"
+    if (input$Color == TRUE){color1 <- "red"}
+    #data <- data[data$quarter == input$quarter, ]
+    #data <- data %>% drop_na()
+    #data <- data[data$QI == input$KPI ]
+    ggplot(data = data, aes(x = paste0(data$year),y = input$KPI)) + geom_bar(fill = color1, stat = "identity") +
+      labs( x = "Year", y = "KPI") 
+  }
+)
+output$Scatter <- renderPlot({
+  color1 <- "blue"
+  if (input$Color == TRUE){color1 <- "red"}
+  data <- data[data$QI == input$KPI , ]
 
+  data <- data[complete.cases(data["QI"]), ]
+  ggplot(data = data, aes(x = quarter, y = input$KPI )) + geom_bar(stat = "identity", fill = color1, ylab = "KPI")  +
+    labs( x = "Quarter", y = "KPI") + theme(axis.title.y = element_blank())
+})
+
+}
 # ;) ----------------------------------------------------------------------
 
 
 shinyApp(ui, server) #Dashboard
 
-#glimpse(data)
-x <- data["QI"]
-x == "door_to_needle"
-
-
-x <- data %>% filter(QI == "door_to_needle")
 
 
 
-x <- data["QI"]
-x<- x%>% filter(data["QI"]=="door_to_needle")
-ggplot(data = x, aes(x = "door_to_needle")) + geom_bar(fill = "darkblue")
+#x <- data %>% filter(QI == "door_to_needle")
+my_color <- c("red","blue", "yellow")
+
+#data <- na.omit(data)
+ggplot(data = data, aes(x = qual4Gold, y =qual4Gold)) + geom_line()
+
+condition <- rep(c("qual4Gold" , "qual4Diamond" , "qual4Platinum"))
+#ggplot(data=data, aes(fill=condition, y=C_Value, x =))+ geom_bar(position ="stack", stat = "identity")
+ggplot(data = data[data$year == 2018,], aes(y = Value, x=QI)) +geom_bar(stat = "identity")
+ggplot(data = data[data$year == 2018,], aes( x=nameOfAggr,y = Value,)) +geom_bar(stat = "identity")
+#Compare 2 KPI
+ggplot(data = data[data$year == 2018,], aes(x=quarter,y = nameOfAggr[])) +geom_point()
+ggplot(data = data[data$year == 2018,], aes(x=quarter,y = nameOfAggr["door_to_needle"])) +geom_bar(stat="identity")
+#position ="stack", stat = "identity"
+
+#x <- data["QI"]
+#x<- x%>% filter(data["QI"]=="door_to_needle")
+#ggplot(data = x, aes(x = "door_to_needle")) + geom_bar(fill = "darkblue")
 
 
 
@@ -80,10 +93,8 @@ ggplot(data = x, aes(x = "door_to_needle")) + geom_bar(fill = "darkblue")
 #ggplot(data = subset(x,!is.na(subGroupVal)), aes(x=subGroupVal)) + geom_bar(fill="darkblue",width = 0.6) + theme_classic()
        
 
-#QI har mulighederne "door_to_groin", NA, "discharge_any_anticoagulant", "door_to_needle"
 
-#Finding changes in KPIs (use agg_dataNum data frame as a starting point)
-#Clinicians currently using this scenario: 50
+
 #Clinicians produce tables on line plots that show indicators for every month/quarter. 
 #They use this data to observe whether their care quality is improving or deteriorating. 
 #Almost all clinicians (43) do this for only a few selected variables that they identify as 
